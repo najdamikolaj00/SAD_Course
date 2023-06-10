764b67283f23f33d6a92eff79eca3d326f671428
Mikolaj Najda <najda.mikolaj00@gmail.com>
2023-06-10 11:43:17 +0200

    energy consumption analysis finished

--- Code/energy_consumption.r
+++ Code/energy_consumption.r
@@ -1,6 +1,8 @@
 # Wczytanie pakietów
-# install.packages("readxl")
+#if(!require(tseries)){install.packages("tseries", "readxl", "ggplot2")} # nolint
 library(readxl)
+library(ggplot2)
+library(tseries)
 
 # Wczytanie danych z pliku
 data <- read_excel("Data/energy.xlsx")
@@ -10,7 +12,7 @@ print(colnames(data))
 # Usunięcie ostatniego wiersza/podsumowującego
 data <- data[-nrow(data), ]
 
-energy <- data$'Billions of Kilowatt-Hours'
+energy <- data$"Billions of Kilowatt-Hours"
 
 # Obliczenie statystyk
 mean_value <- mean(energy)
@@ -38,38 +40,158 @@ cat("Rozstęp międzykwartylowy:", iqr, "\n")
 cat("Skośność:", skewness, "\n")
 cat("Kurtoza:", kurtosis, "\n")
 
-# Wczytanie pakietu do rysowania wykresów
-# install.packages("ggplot2")
-library(ggplot2)
 
 binwidth <- IQR(energy) / 4
 
 # Histogram
+
+# histogram <- ggplot(data, aes(x = Country)) +
+#   geom_bar(aes(y = energy), stat = "identity", fill = "steelblue", color = "white") + # nolint
+#   labs(x = "Kraje", y = "Zużycie energii netto (mld kWh)") +
+#   ggtitle("Zużycie energii netto w Azji (2003)") +
+#   theme_minimal() +
+#   theme(axis.text.x = element_text(angle = 45, hjust = 1),
+#         plot.title = element_text(hjust = 0.5))
+
+# print(histogram) # nolint
+# ggsave("histogram_energia.pdf", plot = histogram, device = "pdf") # nolint
+
+
+#Pudełko z wąsami
+
+# boxplot <- ggplot(data, aes(y = energy)) +
+#   geom_boxplot(fill = "steelblue", color = "black", width = 0.5, lwd = 1.5, lty = 1) + # nolint
+#   labs(y = "Zużycie energii netto (mld kWh)") +
+#   ggtitle("Pudełko z wąsami - Zużycie energii netto w Azji (2003)") +
+#   theme_minimal() +
+#   theme(panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(), # nolint
+#         axis.line = element_line(), # nolint
+#         plot.title = element_text(hjust = 0.5))
+
+# print(boxplot) # nolint
+# ggsave("boxplot_energia.pdf", plot = boxplot, device = "pdf") # nolint
+
+
+# Testy normalności
+# shapiro_result <- shapiro.test(energy)
+# print(shapiro_result)
+
+# ks_result <- ks.test(energy, "pnorm")
+# print(ks_result)
+
+# jarque_result <- jarque.bera.test(energy)
+# print(jarque_result)
+
+
+# Przekształcenie logarytmiczne
+transformed_log <- log(energy)
+print(transformed_log)
+
 histogram <- ggplot(data, aes(x = Country)) +
-  geom_bar(aes(y = energy), stat = "identity", fill = "steelblue", color = "white") +
+  geom_bar(aes(y = transformed_log), stat = "identity", fill = "steelblue", color = "white") + # nolint
   labs(x = "Kraje", y = "Zużycie energii netto (mld kWh)") +
   ggtitle("Zużycie energii netto w Azji (2003)") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         plot.title = element_text(hjust = 0.5))
 
+#print(histogram) # nolint
+ggsave("histogram_energia_log.pdf", plot = histogram, device = "pdf") # nolint
 
-# Wyświetlanie wykresu
-
-# print(histogram)
-# ggsave("histogram_energia.pdf", plot = histogram, device = "pdf")
-
-
-#Pudełko z wąsami
-boxplot <- ggplot(data, aes(y = energy)) +
-  geom_boxplot(fill = "steelblue", color = "black", width = 0.5, lwd = 1.5, lty = 1) +
+boxplot <- ggplot(data, aes(y = transformed_log)) +
+  geom_boxplot(fill = "steelblue", color = "black", width = 0.5, lwd = 1.5, lty = 1) + # nolint
   labs(y = "Zużycie energii netto (mld kWh)") +
   ggtitle("Pudełko z wąsami - Zużycie energii netto w Azji (2003)") +
   theme_minimal() +
   theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.line = element_line(),
+        panel.grid.minor = element_blank(), # nolint
+        axis.line = element_line(), # nolint
         plot.title = element_text(hjust = 0.5))
 
-print(boxplot)
-ggsave("boxplot_energia.pdf", plot = boxplot, device = "pdf")
+print(boxplot) # nolint
+ggsave("boxplot_energia_log.pdf", plot = boxplot, device = "pdf") # nolint
+
+# Weryfikacja normalności dla przekształcenia logarytmicznego
+shapiro_result <- shapiro.test(transformed_log)
+jarque_result <- jarque.bera.test(transformed_log)
+ks_result <- ks.test(transformed_log, "pnorm")
+
+print("Przekształcenie logarytmiczne:")
+print(shapiro_result)
+print(jarque_result)
+print(ks_result)
+
+# Przekształcenie pierwiastka kwadratowego
+transformed_sqrt <- sqrt(energy)
+
+histogram <- ggplot(data, aes(x = Country)) +
+  geom_bar(aes(y = transformed_sqrt), stat = "identity", fill = "steelblue", color = "white") + # nolint
+  labs(x = "Kraje", y = "Zużycie energii netto (mld kWh)") +
+  ggtitle("Zużycie energii netto w Azji (2003)") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        plot.title = element_text(hjust = 0.5))
+
+#print(histogram) # nolint
+ggsave("histogram_energia_sqrt.pdf", plot = histogram, device = "pdf") # nolint
+
+boxplot <- ggplot(data, aes(y = transformed_sqrt)) +
+  geom_boxplot(fill = "steelblue", color = "black", width = 0.5, lwd = 1.5, lty = 1) + # nolint
+  labs(y = "Zużycie energii netto (mld kWh)") +
+  ggtitle("Pudełko z wąsami - Zużycie energii netto w Azji (2003)") +
+  theme_minimal() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), # nolint
+        axis.line = element_line(), # nolint
+        plot.title = element_text(hjust = 0.5))
+
+print(boxplot) # nolint
+ggsave("boxplot_energia_sqrt.pdf", plot = boxplot, device = "pdf") # nolint
+
+# Weryfikacja normalności dla przekształcenia pierwiastka kwadratowego
+shapiro_result <- shapiro.test(transformed_sqrt)
+jarque_result <- jarque.bera.test(transformed_sqrt)
+ks_result <- ks.test(transformed_sqrt, "pnorm")
+
+print("Przekształcenie pierwiastka kwadratowego:")
+print(shapiro_result)
+print(jarque_result)
+print(ks_result)
+
+# Przekształcenie pierwiastka sześciennego
+transformed_cbrt <- energy^(1/3)
+
+histogram <- ggplot(data, aes(x = Country)) +
+  geom_bar(aes(y = transformed_cbrt), stat = "identity", fill = "steelblue", color = "white") + # nolint
+  labs(x = "Kraje", y = "Zużycie energii netto (mld kWh)") +
+  ggtitle("Zużycie energii netto w Azji (2003)") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        plot.title = element_text(hjust = 0.5))
+
+#print(histogram) # nolint
+ggsave("histogram_energia_cbrt.pdf", plot = histogram, device = "pdf") # nolint
+
+boxplot <- ggplot(data, aes(y = transformed_cbrt)) +
+  geom_boxplot(fill = "steelblue", color = "black", width = 0.5, lwd = 1.5, lty = 1) + # nolint
+  labs(y = "Zużycie energii netto (mld kWh)") +
+  ggtitle("Pudełko z wąsami - Zużycie energii netto w Azji (2003)") +
+  theme_minimal() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), # nolint
+        axis.line = element_line(), # nolint
+        plot.title = element_text(hjust = 0.5))
+
+print(boxplot) # nolint
+ggsave("boxplot_energia_cbrt.pdf", plot = boxplot, device = "pdf") # nolint
+
+# Weryfikacja normalności dla przekształcenia pierwiastka sześciennego
+shapiro_result <- shapiro.test(transformed_cbrt)
+jarque_result <- jarque.bera.test(transformed_cbrt)
+ks_result <- ks.test(transformed_cbrt, "pnorm")
+
+print("Przekształcenie pierwiastka sześciennego:")
+print(shapiro_result)
+print(jarque_result)
+print(ks_result)
