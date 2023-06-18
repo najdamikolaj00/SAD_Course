8048bfcd4e1ee873c051a3fb569e839381ba174f
Mikolaj Najda <najda.mikolaj00@gmail.com>
2023-06-18 09:48:12 +0200

    iris dataset multivariate variables normal distribution analysis

--- Code/iris_tests.r
+++ Code/iris_tests.r
@@ -1,9 +1,8 @@
 #Data source: https://archive.ics.uci.edu/dataset/53/iris
-# Wczytanie bibliotek
+
 #install.packages("mvnormtest")
 library(mvnormtest)
 
-# Wczytanie danych z pliku txt
 data <- read.csv("Data/iris.txt", header = TRUE, sep = ",")
 
 setosa <- data[1:50, ]
@@ -13,13 +12,13 @@ virginica <- data[101:150, ]
 columns <- c("sepal.length", "sepal.width", "petal.length", "petal.width")
 
 setosa_result <- mvnormtest::mshapiro.test(t(setosa[, columns]))
-print("Gatunek setosa:")
+print("Class setosa:")
 print(setosa_result)
 
 versicolor_result <- mvnormtest::mshapiro.test(t(versicolor[, columns]))
-print("Gatunek versicolor:")
+print("Class versicolor:")
 print(versicolor_result)
 
 virginica_result <- mvnormtest::mshapiro.test(t(virginica[, columns]))
-print("Gatunek virginica:")
+print("Class virginica:")
 print(virginica_result)
